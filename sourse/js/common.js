a427efb3c4a2324e7fa7f1bfe879f02fec013e03
luckyone <luckyone1221@gmail.com>
2020-10-21 16:05:04 +0300

    sett up container

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -244,7 +244,7 @@ function eventHandler() {
 	// добавляет подложку для pixel perfect
 	var x = window.location.host;
 	let screenName;
-	screenName = '01-1024.png';
+	screenName = '01-375.png';
 	if (screenName && x === "localhost:3000") {
 		$(".footer").after(`<div class="pixel-perfect" style="background-image: url(screen/${screenName});"></div>`);
 	}
