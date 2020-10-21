72e3b104d11bc41b3ff296cd820d25b55a92a978
luckyone <luckyone1221@gmail.com>
2020-10-21 15:59:14 +0300

    made headerblock

--- public/js/common.js
+++ public/js/common.js
@@ -256,7 +256,7 @@ function eventHandler() {
 
 	var x = window.location.host;
 	var screenName;
-	screenName = '01-1440.png';
+	screenName = '01-1024.png';
 
 	if (screenName && x === "localhost:3000") {
 		$(".footer").after("<div class=\"pixel-perfect\" style=\"background-image: url(screen/".concat(screenName, ");\"></div>"));
